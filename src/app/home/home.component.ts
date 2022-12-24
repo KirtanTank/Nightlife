5f11afe746f39f6b162483908718cfa900d77dd8
Kirtan Tank <tankkirtan5@gmail.com>
2022-12-25 01:49:19 +0530

    added Radio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import axios, { Axios } from 'axios';
+import { PassThrough } from 'stream';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +17,14 @@ export class HomeComponent implements OnInit {
   //tmdb poster
   tmdb_poster : any = "https://image.tmdb.org/t/p/w500/";
 
-  //TMDB API
+  //tmdb API
   tmdb_obj : any;
   tmbdArray : any = [];
   tmdb_movie_array : any = [];
+  tmdb_tv_array : any = []
+  tmdb_scrap_array : any = [];
+  tmdb_new_array : any = []
+  show_array : any;
 
   id : any;
   year : any;
@@ -41,6 +46,21 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  radioValue(){
+    const movies = <HTMLInputElement> document.getElementById('radio_input1');
+    const tv = <HTMLInputElement> document.getElementById('radio_input2');
+    const all = <HTMLInputElement> document.getElementById('radio_input3');
+    if(all.checked){
+      this.show_array = this.tmdb_new_array;
+    }
+    else if(tv.checked){
+      this.show_array = this.tmdb_tv_array;
+    }
+    else if(movies.checked){
+      this.show_array = this.tmdb_movie_array;
+    }
+  }
+
   LoadMovie(searchTerm: any){
     searchTerm = this.search;
     this.description = false;
@@ -51,24 +71,11 @@ export class HomeComponent implements OnInit {
 
       const multiURL = `https://api.themoviedb.org/3/search/multi?api_key=1315531f53bb88b9f3c93447893c4b66&language=en-US&query=${searchTerm}&page=1&include_adult=false`
 
-
-      // Loading Animation
-      const a = document.querySelector('.load--hidden');
-      a?.classList.remove("load--hidden");
-      a?.classList.add("load");
-
-      setTimeout(() => {
-        a?.classList.remove("load");
-        a?.classList.add("load--hidden");
-      }, 3000);
-      a?.addEventListener("transitioned", ()=>{
-        document.body.removeChild(a);
-      });
+      this.loadAnimation();
 
       setTimeout(() => {
         // The tmdb api call
         axios.get(multiURL).then(response => {
-          // tmdb
           this.tmdb_obj = response;
           this.tmbdArray = response.data.results;
 
@@ -76,45 +83,70 @@ export class HomeComponent implements OnInit {
             if(this.tmbdArray[m].media_type === 'movie'){
               this.tmdb_movie_array.push(this.tmbdArray[m]);
             }
-          }
-          // No movie
-          const newClass = document.querySelector(".noMovie");
-            if(this.tmdb_movie_array.length == 0){
-              setTimeout(() => {
-                newClass?.classList.add("noMovie--show");
-              }, 500);
+            else if(this.tmbdArray[m].media_type === 'tv'){
+              this.tmdb_tv_array.push(this.tmbdArray[m]);
             }
             else{
-              newClass?.classList.remove("noMovie--show");
-            }  
-          console.log(this.tmdb_movie_array);
+              this.tmdb_scrap_array.push(this.tmbdArray[m]);
+            }
+          }
+          // Combining Two Arrays
+          this.tmdb_new_array = this.tmdb_movie_array.concat(this.tmdb_tv_array);
+          // No movie
+          const newClass = document.querySelector(".noMovie");
+          if(this.tmdb_new_array.length == 0){
+            setTimeout(() => {
+              newClass?.classList.add("noMovie--show");
+            }, 500);
+          }
+          else{
+            newClass?.classList.remove("noMovie--show");
+          }
+          // console.log(this.tmbdArray);  
+          // console.log(this.tmdb_new_array);
+          // console.log(this.tmdb_movie_array);
+          // console.log(this.tmdb_tv_array);
+          // console.log(this.tmdb_scrap_array);
         }).catch(err => {
           console.log(err);
         });
       }, 3000);
+      this.tmdb_new_array.splice(0, this.tmdb_new_array.length);
       this.tmdb_movie_array.splice(0, this.tmdb_movie_array.length);
+      this.tmdb_tv_array.splice(0, this.tmdb_tv_array.length);
       console.log(this.tmdb_movie_array.length);
     }
     else{
-      const notify = document.querySelector(".notification");
-      const searchbar = document.querySelector(".search"); 
-      notify?.classList.add("notification--show");
-      searchbar?.classList.add("shaking")
-      setTimeout(() => {
-        notify?.classList.remove("notification--show");
-        searchbar?.classList.remove("shaking");
-      }, 3000);
+      this.noSearch();
     }
-
   }
 
-  // enter = document.querySelector("search")?.addEventListener("keydown", (e) => {
-  //   let keyboardEvent = <KeyboardEvent> e;
-  //   if(keyboardEvent.key === 'Enter'){
-  //     this.LoadMovie(this.search);
-  //   }
-  // });
-  
+  loadAnimation(){
+    // Loading Animation
+    const a = document.querySelector('.load--hidden');
+    a?.classList.remove("load--hidden");
+    a?.classList.add("load");
+
+    setTimeout(() => {
+      a?.classList.remove("load");
+      a?.classList.add("load--hidden");
+    }, 3000);
+    a?.addEventListener("transitioned", ()=>{
+      document.body.removeChild(a);
+    });
+  }
+
+  noSearch(){
+    const notify = document.querySelector(".notification");
+    const searchbar = document.querySelector(".search"); 
+    notify?.classList.add("notification--show");
+    searchbar?.classList.add("shaking")
+    setTimeout(() => {
+      notify?.classList.remove("notification--show");
+      searchbar?.classList.remove("shaking");
+    }, 3000);
+  }
+
   pop(){
     this.description = true;
     for(let p = 0 ; p < this.tmdb_movie_array.length ; p++){
